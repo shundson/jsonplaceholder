bc4c903c883462dfb11537d210511c9e1dc2c20b
shundson <82779571+shundson@users.noreply.github.com>
2021-04-28 05:31:31 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import "./styles.css";
+
+export default function App() {
+  const onClickUsers = () => {
+    alert("users");
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const onClickUser1 = () => {
+    alert("user1");
+    axios
+      .get("https://jsonplaceholder.typicode.com/users?id=1")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="App">
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>user1</button>
+    </div>
+  );
+}
